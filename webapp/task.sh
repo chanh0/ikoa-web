ec944336b72ffe7b2eee51a3be414aa6a605c784
chanh0 <chanh0@users.noreply.github.com>
2020-10-30 12:21:03 +0800

    Update task.sh

--- webapp/task.sh
+++ webapp/task.sh
@@ -118,7 +118,7 @@ for i in "${!idList[@]}"; do
         echo "${idList[i]},,${TaskId},failed,,,,${TAG},${isMonthly}" >> "$fileName"
         echo "id:${idList[i]} taskid:${TaskId} status:failed tag:${TAG:-None} Monthly:${isMonthly}"
     fi
-    if [[ ($ikoaOutput =~ "已下载" && ($((DownloadCount % 4)) -eq 0 || $codeQuota -lt 45)) || $i -eq $((idListLen - 1)) ]]; then
+    if [[ ($ikoaOutput =~ "已下载" && ($((DownloadCount % 1)) -eq 0 || $codeQuota -lt 45)) || $i -eq $((idListLen - 1)) ]]; then
         sleep 2
         while true
         do
